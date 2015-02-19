288f3ce965d22d4996863a1e6c0675aa8aeadc60
juancarmona26 <juan.carmona@globant.com>
2015-02-19 17:15:53 -0600

    Screen for match results improved

---
+++ app/src/main/java/com/globant/myleague/MatchResultFragment.java
@@ -0,0 +1,20 @@
+package com.globant.myleague;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MatchResultFragment extends Fragment {
+
+    public MatchResultFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_match_result, container, false);
+        return rootView;
+    }
+}
